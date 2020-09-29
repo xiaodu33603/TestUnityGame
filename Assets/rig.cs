14d9beaac63056d30da3bdffdf43796ed60a7ae1
xiaodu33603 <68016007+xiaodu33603@users.noreply.github.com>
2020-09-29 13:58:58 +0800

    Unity工程文件

---
+++ Assets/rig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rig : MonoBehaviour
+{
+    public float speed = 5;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 p = body.position;
+        p.x += moveX * speed * Time.deltaTime;
+        p.y += moveY * speed * Time.deltaTime;
+        body.MovePosition(p);
+    }
+}
---
+++ Assets/人物移动.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 人物移动 : MonoBehaviour
+{
+    public float speed = 5;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 p = body.position;
+        p.x += moveX * speed * Time.deltaTime;
+        p.y += moveY * speed * Time.deltaTime;
+        body.MovePosition(p);
+    }
+}
